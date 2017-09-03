9a078bb53e1a5e6445799b5866f4a2a64627caf7
Rhyan Seymore <9959238@BCS.NET.NZ>
2017-09-04 10:34:49 +1200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9959238_prob2_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
